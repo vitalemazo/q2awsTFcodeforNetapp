d41731367fcbeb2f4d7cee210f2275031848f514
vitale <vitalemazo@gmail.com>
2023-07-23 16:29:15 -0500

    vmazo/q223redo

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role" "role" {
-  name = "q2q-service-role"
+  name = "q3q-service-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -20,37 +20,8 @@ resource "aws_iam_role" "role" {
   })
 }
 
-
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "q2-q-bucket-app-user2023" // change this to a unique name
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket" "westbucket" {
-  provider = aws.west
-  bucket   = "q2-q-westbucket-app-user2023"
-  acl      = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 resource "aws_codepipeline" "pipeline" {
-  name     = "q2_q_pipeline"
+  name     = "q3_q_pipeline"
   role_arn = aws_iam_role.pipeline.arn
 
 
@@ -80,7 +51,7 @@ resource "aws_codepipeline" "pipeline" {
       configuration = {
 
         ConnectionArn    = "arn:aws:codestar-connections:us-east-1:944723394512:connection/72560fc3-ca96-4134-97f2-30275faf451a"
-        FullRepositoryId = "vitalemazo/MyQ2SamplewebApp"
+        FullRepositoryId = "vitalemazo/Myq3SamplewebApp"
         BranchName       = "main"
         #OAuthToken = "***"
       }
@@ -117,28 +88,18 @@ resource "aws_codepipeline" "pipeline" {
       region          = "us-west-2" # Limitation need to go to another region cross-region action
 
       configuration = {
-        ApplicationName = aws_elastic_beanstalk_application.q2_q_app.name
+        ApplicationName = aws_elastic_beanstalk_application.q3_q_app.name
         EnvironmentName = aws_elastic_beanstalk_environment.env.name
       }
     }
   }
 
 
-
-
-
-
-
-
-
-
-
-
 }
 
 
 resource "aws_codebuild_project" "project" {
-  name        = "q2_q_project"
+  name        = "q3_q_project"
   description = "Build project for Windows"
 
   source {
@@ -176,7 +137,7 @@ resource "aws_codebuild_project" "project" {
 
 
 resource "aws_iam_role" "pipeline" {
-  name = "q2_q_pipeline-role"
+  name = "q3_q_pipeline-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -193,7 +154,7 @@ resource "aws_iam_role" "pipeline" {
 }
 
 resource "aws_iam_role" "codebuild" {
-  name = "q2_q_codebuild-role"
+  name = "q3_q_codebuild-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -212,17 +173,17 @@ resource "aws_iam_role" "codebuild" {
   # e.g., AWSCodePipelineFullAccess, AmazonS3FullAccess, etc.
 }
 
-resource "aws_elastic_beanstalk_application" "q2_q_app" {
-  name = "q2_q_application"
+resource "aws_elastic_beanstalk_application" "q3_q_app" {
+  name = "q3_q_application"
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
-  name = "q2-q-instance-profile"
+  name = "q3-q-instance-profile"
   role = aws_iam_role.instance_role.name
 }
 
 resource "aws_iam_role" "instance_role" {
-  name = "q2-q-instance-role"
+  name = "q3-q-instance-role"
 
 
 
@@ -241,8 +202,8 @@ resource "aws_iam_role" "instance_role" {
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
-  name                = "q2qenvironment"
-  application         = aws_elastic_beanstalk_application.q2_q_app.name
+  name                = "q3qenvironment"
+  application         = aws_elastic_beanstalk_application.q3_q_app.name
   solution_stack_name = "64bit Windows Server 2019 v2.11.6 running IIS 10.0" ### another stack with dotnet4.8
 
   setting {
@@ -252,11 +213,8 @@ resource "aws_elastic_beanstalk_environment" "env" {
   }
 }
 
-
-
-
 resource "aws_iam_policy" "s3_upload_policy" {
-  name        = "q2_q_s3_upload_policy"
+  name        = "q3_q_s3_upload_policy"
   description = "Policy to allow CodeBuild to upload to S3 bucket"
 
   policy = jsonencode({
@@ -268,14 +226,13 @@ resource "aws_iam_policy" "s3_upload_policy" {
         ]
         Effect = "Allow"
         Resource = [
-          "arn:aws:s3:::q2-q-bucket-app-user2023/*",
-          "arn:aws:s3:::q2-q-bucket-app-user2023"
+          "arn:aws:s3:::q3-q-bucket-app-user2023/*",
+          "arn:aws:s3:::q3-q-bucket-app-user2023"
         ]
       },
     ]
   })
 }
-
 
 resource "aws_iam_role_policy_attachment" "s3_upload_policy_codebuild" {
   role       = aws_iam_role.codebuild.name
---
+++ s3buckets.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "q3-q-bucket-app-user2023" // change this to a unique name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "westbucket" {
+  provider = aws.west
+  bucket   = "q3-q-west02bucket-app-user2023"
+  acl      = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
